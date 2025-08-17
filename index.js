851314d9e073643c8f6f3de74ad7aa988d3bd657
hokageCV <chaitanyavaru@gmail.com>
2025-08-17 20:01:17 +0530

    Fetch structure of site

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 import { OpenAI } from 'openai';
 import { exec } from 'child_process';
 
+import fs from 'fs';
+
 let TOOL_MAP = {
-  executeCommand: executeCommand,
+  executeCommand,
+  fetchAndSaveHtml,
 };
 
 let client = new OpenAI();
 
 async function main() {
   let SYSTEM_PROMPT = `
+    You are an AI assistant who works on START, THINK and OUTPUT format.
+    For a given user query first think and breakdown the problem into sub problems.
+    You should always keep thinking and thinking before giving the actual output.
+
+    Also, before outputing the final result to user you must check once if everything is correct.
+    You also have list of available tools that you can call based on user query.
+
+    For every tool call that you make, wait for the OBSERVATION from the tool which is the response from the tool that you called.
+
+    Available Tools:
+    - executeCommand(command: string): Takes a linux / unix command as arg and executes the command on user's machine and returns the output
+    - fetchAndSaveHtml(url: string): curls the given site, and saves the response into a file
+
+    Rules:
+    - Strictly follow the output JSON format
+    - Always follow the output in sequence that is START, THINK, OBSERVE and OUTPUT.
+    - Always perform only one step at a time and wait for other step.
+    - Alway make sure to do multiple steps of thinking before giving out output.
+    - For every tool call always wait for the OBSERVE which contains the output from tool
+
+    Output JSON Format:
+    { "step": "START | THINK | OUTPUT | OBSERVE | TOOL" , "content": "string", "tool_name": "string", "input": "STRING" }
+
+    Example:
+    User: make clone of https://google.com
+    ASSISTANT: { "step": "START", "content": "The user wants to clone the given site" }
+    ASSISTANT: { "step": "THINK", "content": "Let me see if there is any available tool for this query" }
+    ASSISTANT: { "step": "THINK", "content": "I see that there is a tool available that fetches data for a site" }
+    ASSISTANT: { "step": "THINK", "content": "I need to call fetchAndSaveHtml for given url" }
+    ASSISTANT: { "step": "TOOL", "input": "https://google.com", "tool_name": "fetchAndSaveHtml" }
+    DEVELOPER: { "step": "OBSERVE", "content": "Website content is fetched and saved into a local file." }
+    ASSISTANT: { "step": "THINK", "content": "Great, I got the website details for the site." }
+    ASSISTANT: { "step": "OUTPUT", "content": "Website cloned successfully." }
   `;
 
   let messages = [
     { role: 'system', content: SYSTEM_PROMPT, },
     {
       role: 'user',
-      content: "",
+      content: `
+        - site: 'https://www.google.com'
+        - make clone of above site.
+      `,
     },
   ];
 
@@ -88,4 +127,13 @@ async function executeCommand(cmd = '') {
       }
     });
   });
+}
+
+async function fetchAndSaveHtml(url = '') {
+  let response = await fetch(url)
+  let data = await response.text()
+
+  fs.writeFileSync('clone.html', data)
+
+  return 'HTML data fetched.'
 }
